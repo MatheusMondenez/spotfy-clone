a80426226873f1d5ec208398ad3c122cb5b80daa
Matheus Mondenez <matheusmondenez@gmail.com>
2019-01-14 14:28:43 -0200

    Lidando com erros

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Wrapper, Container, Content } from "./styles/components";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import Player from "./components/Player";
+import ErrorBox from "./components/ErrorBox";
 import Routes from "./routes";
 import "./config/reactotron";
 import store from "./store";
@@ -19,6 +20,7 @@ const App = () => (
           <Container>
             <Sidebar />
             <Content>
+              <ErrorBox />
               <Header />
               <Routes />
             </Content>
