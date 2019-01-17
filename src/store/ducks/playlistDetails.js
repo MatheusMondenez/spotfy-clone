2d0a267d22c5a5ce20e4174a7d933e08a5766f3a
Matheus Mondenez <matheusmondenez@gmail.com>
2019-01-17 18:52:12 -0200

    Removendo dados fixos

--- src/store/ducks/playlistDetails.js
+++ src/store/ducks/playlistDetails.js
@@ -4,34 +4,7 @@ export const Types = {
 };
 
 const INITIAL_STATE = {
-  data: {
-    id: 4,
-    title: "Só elas",
-    description: "As melhores cantoras POP reunidas em uma playlist pra você.",
-    thumbnail: "https://s3-sa-east-1.amazonaws.com/gonative/linkin_park.jpg",
-    songs: [
-      {
-        id: 3,
-        title: "Points of Authority",
-        author: "Linkin Park",
-        album: "Hybrid Theory",
-        file: "https://s3-sa-east-1.amazonaws.com/gonative/4.mp3",
-        thumbnail:
-          "https://s3-sa-east-1.amazonaws.com/gonative/linkin_park.jpg",
-        playlistId: 4
-      },
-      {
-        id: 4,
-        title: "Crawling",
-        author: "Linkin Park",
-        album: "Hybrid Theory",
-        file: "https://s3-sa-east-1.amazonaws.com/gonative/5.mp3",
-        thumbnail:
-          "https://s3-sa-east-1.amazonaws.com/gonative/linkin_park.jpg",
-        playlistId: 4
-      }
-    ]
-  },
+  data: {},
   loading: false
 };
 
--- src/store/ducks/playlists.js
+++ src/store/ducks/playlists.js
@@ -4,32 +4,7 @@ export const Types = {
 };
 
 const INITIAL_STATE = {
-  data: [
-    {
-      "id": 1,
-      "title": "Saudade das antigas",
-      "description": "Músicas antigas que vão te lembrar dos melhores momentos.",
-      "thumbnail": "https://s3-sa-east-1.amazonaws.com/gonative/linkin_park.jpg"
-    },
-    {
-      "id": 2,
-      "title": "In Too Deep",
-      "description": "As melhores músicas do POP clássico em uma única playlist.",
-      "thumbnail": "https://s3-sa-east-1.amazonaws.com/gonative/foo_fighters.jpg"
-    },
-    {
-      "id": 3,
-      "title": "Salad Days",
-      "description": "Animar seu dia nunca foi tão fácil, ouça essas músicas e tenha um dia mais feliz!",
-      "thumbnail": "https://s3-sa-east-1.amazonaws.com/gonative/alok.jpg"
-    },
-    {
-      "id": 4,
-      "title": "Só elas",
-      "description": "As melhores cantoras POP reunidas em uma playlist pra você.",
-      "thumbnail": "https://s3-sa-east-1.amazonaws.com/gonative/linkin_park.jpg"
-    }
-  ],
+  data: [],
   loading: false
 };
 
